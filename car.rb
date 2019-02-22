d5429247dc194880416de6a2b2d34d84eb8b23ce
Chad Logsdon <cavejohnson93@gmail.com>
2019-02-22 13:59:45 -0600

    refactored to add inheritance and super keyword

--- car.rb
+++ car.rb
@@ -1,40 +1,47 @@
-class Car
-  attr_accessor :mileage, :color
+class Vehicle
+  attr_accessor :color, :honk_horn
+
+  def initialize(color)
+    @color=color
+  end
+
+  def honk_horn
+    puts "Honk Honk"
+  end
+end
+
+
+class Car<Vehicle
+  attr_accessor :mileage
 
   def initialize(mileage,color)
+    super(color)
     @mileage=mileage
-    @color=color
   end
   def drive(miles)
     puts "*Drives #{miles} miles*"
     @mileage = (@mileage + miles)
   end
-  def honk_horn
-    puts "HONK!"
-  end
 end
 
-# class Bike
-# def color
-#   return "Blue"
-# end
-# def honk_horn
-#   puts "RING RING"
-# end
-# def ride
-#   puts "*Rides bike like only Vin Diesel could*"
-# end
-# end
+class Bike<Vehicle
+
+def ride
+  puts "*Rides bike like only Vin Diesel could*"
+end
+end
 
 
 mycar = Car.new(5600,"Red")
-# puts mycar.mileage
-# puts mycar.color
-# mycar.honk_horn
+puts mycar.mileage
+puts mycar.color
+mycar.honk_horn
+
 puts mycar.mileage
 mycar.drive(5)
 puts mycar.mileage
-# vinbike = Bike.new
-# puts vinbike.color
-# vinbike.honk_horn
-# vinbike.ride
+
+vinbike = Bike.new("Blue")
+puts vinbike.color
+vinbike.honk_horn
+vinbike.ride
